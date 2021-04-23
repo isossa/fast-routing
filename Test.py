b88f0ab8fca2663ea9b6693041ce6f05465faada
isossa <sossajeovanis@gmail.com>
2021-04-23 13:33:21 -0500

    Added Test.py.

--- Test.py
+++ Test.py
@@ -11,24 +11,33 @@ from dataframeutils import standardize_dataframe_columns
 # Load addresses
 address_location = '..\\ISC Project\\ISC Project Code\\RandomAddresses.xlsx'
 data = pd.read_excel(address_location)
-standardize_dataframe_columns(data)
 
-subset_data = data[:5]
+# Clean data
+temp_df = data.select_dtypes(include='object')
+data[temp_df.columns] = temp_df.apply(lambda x: x.str.strip())
+data.drop_duplicates(inplace=True, ignore_index=True)
+standardize_dataframe_columns(data)
+print(f'Reading {len(data)} addresses.')
 
 # Get geocodes
-addresses = Address.build_addresses(subset_data)
-geocodes = [add.coordinates_as_string() for add in addresses]
+addresses = Address.build_addresses(data)
+all_geocodes, log = Geocoding.get_geocodes(addresses)
+print(f'Number of geocode retrieves: {len(all_geocodes)}')
+print(f'Number of failed geocoding: {len(log)}')
+geocodes_map = {geocode : index for index, geocode in enumerate(all_geocodes)}
 
-for i, add in enumerate(addresses):
-    display(add, geocodes[i])
-
-# get distance matrix
+# Request distance and duration matrices
 api_key = os.environ['BING_MAPS_API_KEY']
-response = DistanceMatrix.request_matrix(geocodes, api_key, 'driving')
+distance_map, duration_map, response = DistanceMatrix.request_matrix(list(geocodes_map.keys()), api_key, 'driving', size=10)
 
-print(type(response))
+print(f'API call status: {response.request}')
 
-print()
+# Get Display matrices
+distance_matrix = DistanceMatrix.get_matrix(distance_map, geocodes_map)
+duration_matrix = DistanceMatrix.get_matrix(duration_map, geocodes_map)
 
-print(response)
+print("\nDistance Matrix")
+display(pd.DataFrame(distance_matrix))
 
+print("\nDuration Matrix")
+display(pd.DataFrame(duration_matrix))
