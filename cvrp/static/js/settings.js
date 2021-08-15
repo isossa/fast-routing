8229b595f6580c61b50d6636f5231c4a920ca1c8
Isidore Sossa <sossajeovanis@gmail.com>
2021-08-15 00:10:02 -0400

    Added background process handler.

--- cvrp/static/js/settings.js
+++ cvrp/static/js/settings.js
@@ -1,4 +1,9 @@
 
+$(document).ready(function(){
+    document.getElementById('btn-spinner').classList.add('not-visible');
+    document.getElementById('btn-load').classList.remove('not-visible');
+});
+
 $(document).on('click', '#btn-load', function(){
     document.getElementById('btn-spinner').classList.remove('not-visible');
     document.getElementById('btn-load').classList.add('not-visible');
